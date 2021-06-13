a8edbfa60d7f8bf1053d66d5459bac08e8b36011
Flavio001 <flaviodutra18@hotmail.com>
2021-06-13 15:12:31 -0300

    Adiçao de arquivos e criaçao do primeiro componente - SafeEnvironment (Pagina Segura)

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
+
+
+export default function Home() {
+  return (
+    <div>
+    <SafeEnvironment />
+    ola mundo tudo bem com você
+    </div>
+  )
+}
