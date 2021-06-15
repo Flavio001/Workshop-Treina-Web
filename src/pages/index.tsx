881feded4026bdfd8a47460a7e0ab04698e8ce6c
Flavio001 <flaviodutra18@hotmail.com>
2021-06-14 23:40:51 -0300

    Adiçao de elemento input de busca por CPF da pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@material-ui/core";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import { TextFieldStyled } from "ui/components/inputs/TextField/TextField.style";
+import { Typography } from "@material-ui/core";
+
 import UserInformation from "ui/components/user-information/UserInformation";
+
 
 export default function Home() {
   return (
@@ -10,6 +15,12 @@ export default function Home() {
         titulo={"Conheça nossos profissionais"}
         subtitulo={"Contrate um de nossos colaboradores agora"}
       />
+      <TextFieldStyled variant={'outlined'} label={'Digite o seu CEP'} fullWidth />
+
+      <Typography color={'error'}>CEP Inválido</Typography>
+
+      <Button variant={'contained'} color={'secondary'} sx={{width: '220px'}}>Buscar</Button>
+      
       <UserInformation 
       name={'Akira Hanashiro'}
       picture={'https://github.com/hanashiro.png'}
---
+++ src/ui/components/inputs/TextField/TextFieldMask/TextFieldMask.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import InputMask from 'react-input-mask'
+import { TextFieldStyled } from "ui/components/inputs/TextField/TextField.style";
+import {OutlinedTextFieldProps} from '@material-ui/core'
+
+export interface TextFieldMaskProps extends OutlinedTextFieldProps{
+    mask: string
+}
+
+const TextFieldMask: React.FC<TextFieldMaskProps> = (props) => {
+  return (
+    <InputMask mask={props.mask}>
+      {() => {
+        return <TextFieldStyled></TextFieldStyled>;
+      }}
+    </InputMask>
+  );
+};
+export default TextFieldMask;
